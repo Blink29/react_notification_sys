9fec9d3093f57d36c5b1395d2851c6c35b9cd5b1
Blink29 <purukumar2905@gmail.com>
2023-07-15 12:49:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import JokeList from "./JokeList";
+import SingleJoke from "./SingleJoke";
 
 function App() {
+  const URL = "httpss://official-joke-api.appspot.com/random_joke"
+
+  const [jokes, setJokes] = useState([])
+  const [visibleJokes, setVisibleJokes] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [fetchError, setFetchError] = useState(null)
+
+  useEffect(() => {
+    const fetchJoke = async () => {
+      setIsLoading(true)
+      try {
+        const response = await axios.get(URL)
+        const jokeData = [response.data]
+        setJokes(jokeData)
+      } catch(err) {
+          console.log(err)
+          setFetchError(err.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchJoke() 
+  }, [jokes, setJokes])
+
+  // useEffect(() => {
+
+  // }, [jokes]);
+  useEffect(() => {
+      setVisibleJokes((prevJokes) => {
+        const startIndex = prevJokes.length >= 5 ? prevJokes.length - 5 : 0;
+        return jokes.slice(startIndex, startIndex + 5);
+      });
+  }, [jokes]);
+
+  useEffect(() => {
+    console.log(visibleJokes);
+  }, [visibleJokes]);
+
+  useEffect(() => {
+    
+  }, [jokes])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<JokeList 
+        visibleJokes={visibleJokes}
+        isLoading={isLoading}
+      />} />
+      <Route path=":id" element={<SingleJoke />} />
+    </Routes>
   );
 }
 
